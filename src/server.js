edadae468bbda885fe2716aab75d57ef5559e563
Sebastian Latkolic <latkolicsebastian@gmail.com>
2021-01-14 14:05:42 +0200

    changed port

--- src/server.js
+++ src/server.js
@@ -87,4 +87,4 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+app.listen(5000, () => console.log('Listening on port 5000'));
